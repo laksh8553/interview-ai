40f2bda70c1566cc54eeca1ed6b0105024e429d4
Laksh8553 <Lakshay22122004@gmail.com>
2026-05-01 19:06:39 +0530

    added puppeteer for downloading ai generated pdf

--- Frontend/src/features/interview/hooks/useInterview.js
+++ Frontend/src/features/interview/hooks/useInterview.js
@@ -1,4 +1,4 @@
-import {generateInterviewReport,getAllInterviewReports,getInterviewReportById} from "../services/interview.api"
+import {generateInterviewReport,getAllInterviewReports,getInterviewReportById, generateResumePdf} from "../services/interview.api"
 import { useContext, useEffect } from "react";
 import { InterviewContext } from "../interview.context";
 import { useParams } from "react-router";
@@ -62,6 +62,26 @@ export const useInterview = ()=>{
       return response.interviewReports;
     };
 
+    const getResumePdf = async (interviewReportId) => {
+      setLoading(true);
+      let response = null;
+      try {
+        response = await generateResumePdf({ interviewReportId });
+        const url = window.URL.createObjectURL(
+          new Blob([response], { type: "application/pdf" }),
+        );
+        const link = document.createElement("a");
+        link.href = url;
+        link.setAttribute("download", `resume_${interviewReportId}.pdf`);
+        document.body.appendChild(link);
+       link.click();
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
     useEffect(() => {
       if (interviewId) {
         getReportById(interviewId);
@@ -70,5 +90,5 @@ export const useInterview = ()=>{
       }
     }, [interviewId]);
    
-    return { loading, report, reports, generateReport, getReportById, getReports}
+    return { loading, report, reports, generateReport, getReportById, getReports, getResumePdf}
 }
