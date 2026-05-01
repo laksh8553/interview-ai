8773f53257177f9ee1a1da00307fa38fa577bac8
Laksh8553 <Lakshay22122004@gmail.com>
2026-05-01 20:12:09 +0530

    added backend url

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 
 
 const api = axios.create({
-    baseURL:"http://localhost:3000",
-    withCredentials: true,
-})
+  baseURL: "https://carrerpilot-ai.onrender.com",
+  withCredentials: true,
+});
 
 
 /**
